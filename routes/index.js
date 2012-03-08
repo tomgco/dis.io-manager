c79a6c8f864cab6e36e1d480c83f86107a04d762
Tom Gallacher <tomgallacher23@gmail.com>
2012-03-08 14:52:52 +0000

    Adding rest routes

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,32 @@
 
 exports.task = {};
 
+
 exports.task.exists = function(req, res, next) {
   res.send(req.params);
   return next();
 };
 
+exports.task.create = function(req, res, next) {
+  res.send(req.params);
+  return next();
+};
+
+exports.task.edit = function(req, res, next) {
+  res.send(req.params);
+  return next();
+};
+
+exports.task.remove = function(req, res, next) {
+  res.send(req.params);
+  return next();
+};
+
+  // server.get('/task/exists', routes.task.exists);
+
+  // server.post('/task/create', routes.task.create);
+
+  // server.put('/task/edit', routes.task.edit);
+
+  // server.del('/task/remove', routes.task.remove);
 // exports.
